b296831f000a5514295b8255e6ec0945b4b5e123
Janghoik <wkdghdlr1@naver.com>
2022-01-02 17:09:13 +0900

    second commit

--- back10989.py
+++ back10989.py
@@ -12,7 +12,7 @@ for j in range(0, N):  # j = 0 1 2 3 4
     for k in range(j + 1, N, 1):  # k= 1 2 3 4
         if (K[indexmin] > K[k]):
             indexmin = k
-
+    print("hello")
     change = K[indexmin]
     K[indexmin] = K[j]
     K[j] = change
